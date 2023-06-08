32e46d3deaff88705fa5677fb9f3c2453a661daa
jellyfish700 <jellyfish.apple200@gmail.com>
2023-06-09 00:19:43 +0900

    変更１

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editaaaaaaaa <code>src/App.js</code> and save to reload.
+          Ed <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
