beaaecdfffb97c13dfeafd133dbd9ae837f80824
jellyfish700 <jellyfish.apple200@gmail.com>
2023-08-12 00:35:30 +0900

    メタ情報

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,14 +4,14 @@ import Card from 'react-bootstrap/Card';
 import image1 from './image/image1.svg'
 import image2 from './image/image2.svg'
 import image3 from './image/image3.svg'
-import image4 from './image/image4.png'
+import image4 from './image/image4.svg'
 const Work = () => {
   return (
     <div>
       <p class="fs-2 tx-p2 mb-5">WORK</p> 
 
-      <Card className="card left card-row">
-        <Card.Img class="image-card" variant="top" src={image1} />
+      <Card className="card left card-row-m">
+        <Card.Img class="image-card image-height-m" variant="top" src={image1} />
         <Card.Body>
           <Card.Title class="fs-3 tx-p3">ポートフォリオ</Card.Title>
           <p class="fs-4 tx-p2">HTML / CSS / JavaScript / React</p>
@@ -19,8 +19,8 @@ const Work = () => {
         </Card.Body>
       </Card>
 
-      <Card className="card left card-row">
-        <Card.Img class="image-card" variant="top" src={image2} />
+      <Card className="card left card-row-m">
+        <Card.Img class="image-card image-height-m" variant="top" src={image2} />
         <Card.Body>
           <Card.Title class="fs-3 tx-p3">学習向けソフトウェア</Card.Title>
           <p class="fs-4 tx-p2">Python / tkinter</p>
@@ -28,21 +28,21 @@ const Work = () => {
         </Card.Body>
       </Card>
 
-      <Card className="card">
-        <Card.Img class="image-card" variant="top" src={image3} />
+      <Card className="card left card-row-l">
+        <Card.Img class="image-card image-height-l" variant="top" src={image3} />
         <Card.Body>
-          <Card.Title class="fs-3 tx-p3">人気な曲を教えてくれる<br></br>チャットbot</Card.Title>
+          <Card.Title class="fs-3 tx-p3">チャットbot</Card.Title>
           <p class="fs-4 tx-p2">Python / janome / tkinter</p>
           <p class="fs-5 tx-p2">アーティスト名を入力するとそのアーティストの人気な曲が出力されるチャットbotを作成しました。入力された文字列を形態素解析するために、janomeを使用しました。描画ライブラリはtkinterを使用しています。</p>
         </Card.Body>
       </Card>
 
-      <Card className="card">
-        <Card.Img class="image-card" variant="top" src={image4} />
+      <Card className="card left card-row-l">
+        <Card.Img class="image-card image-height-l" variant="top" src={image4} />
         <Card.Body>
           <Card.Title class="fs-3 tx-p3">認知症予防の日記サービス</Card.Title>
           <p class="fs-4 tx-p2">aws / Vue / node.js</p>
-          <p class="fs-5 tx-p2"></p>
+          <p class="fs-5 tx-p2">株式会社エムティーアイのインターンで、チーム開発しました。日記の投稿、削除、検索、色やフォントの変更ができます。</p>
         </Card.Body>
       </Card>
 
