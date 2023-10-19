a8790b6edaa0927f70a000556f8d68d0e7d1ea7d
jellyfish700 <jellyfish.apple200@gmail.com>
2023-10-19 18:36:15 +0900

    クラス変更

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <About />
       <Work />
       <Skill />
-      {/* <Contact /> */}
+      <Contact />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -14,16 +14,16 @@ const Header = () => {
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li>
-                <a href='#'><img src={person} alt="" class="icon"/>ABOUT</a>
+                <a href='#about'><img src={person} alt="" class="icon"/>ABOUT</a>
               </li>
               <li>
-                <a href='#'><img src={file} alt="" class="icon"/>WORK</a>
+                <a href='#work'><img src={file} alt="" class="icon"/>WORK</a>
               </li>
               <li>
-                <a href='#'><img src={pc} alt="" class="icon"/>SKILL</a>
+                <a href='#skill'><img src={pc} alt="" class="icon"/>SKILL</a>
               </li>
               <li>
-                <a href='#'><img src={mail} alt="" class="icon"/>CONTACT</a>
+                <a href='#contact'><img src={mail} alt="" class="icon"/>CONTACT</a>
               </li>
             </ul>
           </div>
@@ -32,17 +32,17 @@ const Header = () => {
         <nav class="pc">
           <ul>
             <li>
-              <a href='#'><img src={person} alt="" class="icon"/>ABOUT</a>
+              <a href='#about'><img src={person} alt="" class="icon"/>ABOUT</a>
             </li>
             <li>
-              <a href='#'><img src={file} alt="" class="icon"/>WORK</a>
+              <a href='#work'><img src={file} alt="" class="icon"/>WORK</a>
             </li>
             <li>
-              <a href='#'><img src={pc} alt="" class="icon"/>SKILL</a>
+              <a href='#skill'><img src={pc} alt="" class="icon"/>SKILL</a>
             </li>
           
             <li>
-              <a href='#'><img src={mail} alt="" class="icon"/>CONTACT</a>
+              <a href='#contact'><img src={mail} alt="" class="icon"/>CONTACT</a>
             </li>
           </ul>
         </nav>
